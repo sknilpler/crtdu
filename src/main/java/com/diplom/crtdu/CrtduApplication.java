e2ff43a068cc86cdbf773d276c4dc396b8d4aa2b
sknilpler <nesterov.craft.29@gmail.com>
2023-02-14 14:33:22 +0500

    Initial commit

---
+++ src/main/java/com/diplom/crtdu/CrtduApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.crtdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrtduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplom/crtdu/CrtduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.crtdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrtduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
