bc42b39fdb651384b3bdc8687df8195a19b039fe
sknilpler <nesterov.craft.29@gmail.com>
2023-04-11 15:48:33 +0500

    done kids managment page, working on teachers pages

--- src/main/java/com/diplom/crtdu/controllers/SpecController.java
+++ src/main/java/com/diplom/crtdu/controllers/SpecController.java
@@ -1,10 +1,9 @@
 package com.diplom.crtdu.controllers;
 
 import com.diplom.crtdu.exception.NotFoundException;
-import com.diplom.crtdu.models.Kid;
-import com.diplom.crtdu.models.Parents;
-import com.diplom.crtdu.repo.KidRepository;
-import com.diplom.crtdu.repo.ParentsRepository;
+import com.diplom.crtdu.models.*;
+import com.diplom.crtdu.repo.*;
+import com.diplom.crtdu.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -15,12 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
@@ -29,12 +28,44 @@ public class SpecController {
 
     private long kidEditId;
     private Kid editedKid;
+    private long teachEditId;
+    private Teacher editedTeach;
 
     @Autowired
     private KidRepository kidRepository;
-
     @Autowired
     private ParentsRepository parentsRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private KrujokRepository krujokRepository;
+
+
+//    public static String cyrillicToLatin(String input) {
+//        // Массив для хранения заменяемых символов
+//        String[][] replacements = {
+//                {"а", "a"}, {"б", "b"}, {"в", "v"}, {"г", "g"}, {"д", "d"}, {"е", "e"}, {"ё", "yo"}, {"ж", "zh"},
+//                {"з", "z"}, {"и", "i"}, {"й", "y"}, {"к", "k"}, {"л", "l"}, {"м", "m"}, {"н", "n"}, {"о", "o"},
+//                {"п", "p"}, {"р", "r"}, {"с", "s"}, {"т", "t"}, {"у", "u"}, {"ф", "f"}, {"х", "h"}, {"ц", "c"},
+//                {"ч", "ch"}, {"ш", "sh"}, {"щ", "sch"}, {"ъ", ""}, {"ы", "y"}, {"ь", ""}, {"э", "e"}, {"ю", "yu"},
+//                {"я", "ya"}, {"А", "A"}, {"Б", "B"}, {"В", "V"}, {"Г", "G"}, {"Д", "D"}, {"Е", "E"}, {"Ё", "Yo"},
+//                {"Ж", "Zh"}, {"З", "Z"}, {"И", "I"}, {"Й", "Y"}, {"К", "K"}, {"Л", "L"}, {"М", "M"}, {"Н", "N"},
+//                {"О", "O"}, {"П", "P"}, {"Р", "R"}, {"С", "S"}, {"Т", "T"}, {"У", "U"}, {"Ф", "F"}, {"Х", "H"},
+//                {"Ц", "C"}, {"Ч", "Ch"}, {"Ш", "Sh"}, {"Щ", "Sch"}, {"Ъ", ""}, {"Ы", "Y"}, {"Ь", ""}, {"Э", "E"},
+//                {"Ю", "Yu"}, {"Я", "Ya"}
+//        };
+//
+//        // Заменяем символы в строке
+//        for (String[] replacement : replacements) {
+//            input = input.replace(replacement[0], replacement[1]);
+//        }
+//
+//        return input;
+//    }
 
     @GetMapping("/spec/list-kids")
     public String openKidsListPage(Model model) {
@@ -43,23 +74,31 @@ public class SpecController {
     }
 
     @GetMapping("/spec/kid-add")
-    public String openKidAddPage() {
+    public String openKidAddPage(Model model) {
+        //model.addAttribute("formTarget", "_blank");
         return "spec/kids-add";
     }
 
-    @PostMapping("/spec/kid-add")
-    public String addKid(@RequestParam String surname,
-                         @RequestParam String name,
-                         @RequestParam String patronymic,
-                         @RequestParam String birthday,
-                         @RequestParam String sex,
-                         @RequestParam String grazhdanstvo,
-                         @RequestParam String adres,
-                         @RequestParam String phone,
-                         @RequestParam String school,
-                         @RequestParam String klas,
+    @PostMapping("/spec/kid-add/{surname}/{name}/{patronymic}/{birthday}/{sex}/{grazhdanstvo}/{adres}/{phone}/{school}/{klas}/{username}/{password}")
+    public String addKid(@PathVariable("surname") String surname,
+                         @PathVariable("name") String name,
+                         @PathVariable("patronymic") String patronymic,
+                         @PathVariable("birthday") String birthday,
+                         @PathVariable("sex") String sex,
+                         @PathVariable("grazhdanstvo") String grazhdanstvo,
+                         @PathVariable("adres") String adres,
+                         @PathVariable("phone") String phone,
+                         @PathVariable("school") String school,
+                         @PathVariable("klas") String klas,
+                         @PathVariable("username") String username,
+                         @PathVariable("password") String password,
                          Authentication authentication) {
 
+        Role role = roleRepository.findByName("ROLE_KID");
+        User user = new User(username, password, "kid", surname, name, patronymic, password);
+        user.setRoles(Collections.singleton(role));
+        userService.saveUser(user);
+        log.warn("ADD new User: {}", user);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Date dateBirth = new Date();
         try {
@@ -70,8 +109,10 @@ public class SpecController {
         }
         boolean gender = sex.equals("male");
         Kid kid = new Kid(surname, name, patronymic, dateBirth, gender, grazhdanstvo, adres, phone, school, klas);
+        kid.setUsername(username);
         kidRepository.save(kid);
         log.warn("ADD new Kid: {}", kid);
+
         return "redirect:/spec/list-kids";
     }
 
@@ -83,12 +124,6 @@ public class SpecController {
         return "spec/list-kids :: info-kids";
     }
 
-//    @PostMapping("/spec/list-kids/{id}/remove")
-//    public String kidDelete(@PathVariable(value = "id") long id, Model model) {
-//        Kid kid = kidRepository.findById(id).orElseThrow(() -> new NotFoundException("Kid with id = " + id + " not found on server!"));
-//        kidRepository.deleteById(id);
-//        return "redirect:/spec/list-kids";
-//    }
 
     @GetMapping("/spec/list-kids/edit/{id}")
     public String openEditKid(Model model, @PathVariable("id") Long id) {
@@ -98,7 +133,7 @@ public class SpecController {
         List<Parents> parents = parentsRepository.findByKidsId(kid.getId());
         model.addAttribute("kid", kid);
         model.addAttribute("parents", parents);
-        model.addAttribute("parent", parents.size() > 0 ? parents.get(0) : new Parents("", "", "", "", "",""));
+        model.addAttribute("parent", parents.size() > 0 ? parents.get(0) : new Parents("", "", "", "", "", ""));
         return "spec/kids-edit";
     }
 
@@ -152,7 +187,7 @@ public class SpecController {
                                 @PathVariable("adres") String adres,
                                 @PathVariable("email") String email,
                                 @PathVariable("phone") String phone) {
-        Parents parents = new Parents(surname, name, patronymic, adres, phone,email);
+        Parents parents = new Parents(surname, name, patronymic, adres, phone, email);
         List<Kid> kids = new ArrayList<>();
         kids.add(editedKid);
         parents.setKids(kids);
@@ -193,7 +228,8 @@ public class SpecController {
 
     @PostMapping("/spec/list-kids/del/{id}")
     public String delKid(Model model, @PathVariable("id") Long id) {
-        System.out.println("Deleting kid with ID: " + id);
+        Kid kid = kidRepository.findById(id).orElseThrow(() -> new NotFoundException("Kid with id = " + id + " not found on server!"));
+        log.warn("Deleting Kid: {}", kid);
         List<Parents> p = parentsRepository.findByKidsId(id);
         p.forEach(parents -> {
             if ((parents.getKids().size() == 1) && (Objects.equals(parents.getKids().get(0).getId(), id))) {
@@ -201,8 +237,90 @@ public class SpecController {
                 System.out.println("\tDeleted parents: ID - " + parents.getId() + " FIO - " + parents.getFullFIO());
             }
         });
+        User user = userService.getUserByUsername(kid.getUsername());
+        if (userService.deleteUser(user.getId())) log.warn("\tDeleted user: {}", user);
         kidRepository.deleteById(id);
         return "redirect:/spec/list-kids";
+    }
+
+
+    //---------------- преподаватели ------------------------
+
+    @GetMapping("/spec/list-teachers")
+    public String openTeacherListPage(Model model) {
+        model.addAttribute("teachers", teacherRepository.findAll());
+        return "spec/teacher-list";
+    }
+
+    @GetMapping("/spec/teacher-add")
+    public String openTeacherAddPage(Model model) {
+        return "spec/teacher-add";
+    }
+
+    @PostMapping("/spec/teacher-add/{surname}/{name}/{patronymic}/{doljnost}/{username}/{password}")
+    public String addTeacher(@PathVariable("surname") String surname,
+                         @PathVariable("name") String name,
+                         @PathVariable("patronymic") String patronymic,
+                         @PathVariable("doljnost") String doljnost,
+                         @PathVariable("username") String username,
+                         @PathVariable("password") String password,
+                         Authentication authentication) {
+
+        Role role = roleRepository.findByName("ROLE_TEACHER");
+        User user = new User(username, password, "teacher", surname, name, patronymic, password);
+        user.setRoles(Collections.singleton(role));
+        userService.saveUser(user);
+        log.warn("ADD new User: {}", user);
+        Teacher teach = new Teacher(surname, name, patronymic, doljnost);
+        teach.setUsername(username);
+        teacherRepository.save(teach);
+        log.warn("ADD new Teach: {}", teach);
+
+        return "redirect:/spec/list-teachers";
+    }
+
+
+    @GetMapping("/spec/list-teachers/edit/{id}")
+    public String openEditTeacher(Model model, @PathVariable("id") Long id) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new NotFoundException("Teacher with id = " + id + " not found on server!"));
+        teachEditId = id;
+        editedTeach = teacher;
+        model.addAttribute("teacher", teacher);
+        return "spec/teacher-edit";
+    }
+
+    @PostMapping("/spec/list-teachers/del/{id}")
+    public String delTeacher(Model model, @PathVariable("id") Long id) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new NotFoundException("Teacher with id = " + id + " not found on server!"));
+        log.warn("Deleting Teacher: {}", teacher);
+        User user = userService.getUserByUsername(teacher.getUsername());
+        if (userService.deleteUser(user.getId())) log.warn("\tDeleted user: {}", user);
+        teacherRepository.deleteById(id);
+        return "redirect:/spec/list-teachers";
+    }
+
+    @GetMapping("/spec/list-teachers/info/{id}")
+    public String openInfoTeacher(Model model, @PathVariable("id") Long id) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new NotFoundException("Teacher with id = " + id + " not found on server!"));
+        model.addAttribute("teacher", teacher);
+        model.addAttribute("krujki", krujokRepository.findByTeachersId(id));
+        return "spec/teacher-list :: info-teacher";
+    }
+
+    @PostMapping("/spec/list-teachers/edit/{id}/{surname}/{name}/{patronymic}/{doljnost}")
+    public String saveEditTeacher(Model model, @PathVariable("id") Long id,
+                              @PathVariable("surname") String surname,
+                              @PathVariable("name") String name,
+                              @PathVariable("patronymic") String patronymic,
+                              @PathVariable("doljnost") String doljnost) {
+        Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new NotFoundException("Teacher with id = " + id + " not found on server!"));
+        teacher.setSurname(surname);
+        teacher.setName(name);
+        teacher.setPatronymic(patronymic);
+        teacher.setDoljnost(doljnost);
+        teacherRepository.save(teacher);
+        log.warn("Edit Teacher: {}", teacher);
+        return "redirect:/spec/list-teachers";
     }
 }
 
--- src/main/java/com/diplom/crtdu/repo/KrujokRepository.java
+++ src/main/java/com/diplom/crtdu/repo/KrujokRepository.java
@@ -3,5 +3,8 @@ package com.diplom.crtdu.repo;
 import com.diplom.crtdu.models.Krujok;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface KrujokRepository extends CrudRepository<Krujok,Long> {
+    List<Krujok> findByTeachersId(Long id);
 }
