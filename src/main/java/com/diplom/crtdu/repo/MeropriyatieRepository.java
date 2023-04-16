792da0856a592bb07dc695e8407af89ed7095c87
sknilpler <nesterov.craft.29@gmail.com>
2023-04-17 00:16:32 +0500

    commit home 17.04.23

--- src/main/java/com/diplom/crtdu/repo/MeropriyatieRepository.java
+++ src/main/java/com/diplom/crtdu/repo/MeropriyatieRepository.java
@@ -1,7 +1,23 @@
 package com.diplom.crtdu.repo;
 
 import com.diplom.crtdu.models.Meropriyatie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MeropriyatieRepository extends CrudRepository<Meropriyatie,Long> {
+
+    @Query(value = "SELECT meropriyatie.*\n" +
+            "FROM meropriyatie\n" +
+            "WHERE meropriyatie.data BETWEEN :d1 AND :d2 ORDER BY meropriyatie.data", nativeQuery = true)
+    List<Meropriyatie> findByData(@Param("d1") String date1,
+                                  @Param("d2") String date2);
+
+    @Query(value = "SELECT meropriyatie.*\n" +
+            "FROM meropriyatie\n" +
+            "WHERE meropriyatie.data BETWEEN :d1 AND :d2 ORDER BY meropriyatie.data DESC", nativeQuery = true)
+    List<Meropriyatie> findByDataDesc(@Param("d1") String date1,
+                                      @Param("d2") String date2);
 }
